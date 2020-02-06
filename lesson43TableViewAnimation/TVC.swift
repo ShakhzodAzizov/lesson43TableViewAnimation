d612e11f4879d17c53b5900ab893b529d3ea43c6
Shakhzod <a>
2020-02-06 19:19:24 +0500

    Add Table View Flip animation

---
+++ lesson43TableViewAnimation/TVC.swift
@@ -0,0 +1,27 @@
+//
+//  TVC.swift
+//  lesson43TableViewAnimation
+//
+//  Created by Shakhzodbek on 2/6/20.
+//  Copyright © 2020 Shakhzodbek Azizov. All rights reserved.
+//
+
+import UIKit
+
+class TVC: UITableViewCell {
+
+    
+    @IBOutlet weak var view: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- lesson43TableViewAnimation/ViewController.swift
+++ lesson43TableViewAnimation/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TVC else{
+            return UITableViewCell()
+        }
+        
+    cell.view.backgroundColor = .red
+        
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .transitionFlipFromTop, animations: {
+            cell.transform = .identity
+        }, completion: nil)
+        
+        let redView = cell.viewWithTag(3)!
+        UIView.transition(with: redView, duration: 0.3, options: .transitionFlipFromBottom, animations: {
+            
+        }, completion: nil)
+        
+    }
+}
